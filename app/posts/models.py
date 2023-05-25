8e08eb53b0b55a9634d322a4466f5b2ece39c092
Alexey Bolshakov <avbolshakov@gmail.com>
2023-05-25 17:35:26 +0300

    add tag model

--- app/posts/models.py
+++ app/posts/models.py
@@ -31,3 +31,14 @@ class Post(db.Model):
     
 
     
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    slug = db.Column(db.String(140), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.slug=slugify(self.title)
+
+    def __repr__(self):
+        return f"<Tag id:{self.id}, title: {self.title}>"
