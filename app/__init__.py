1c20b349110d5b0bfce16f5d81379ab6e2252105
Alexey Bolshakov <avbolshakov@gmail.com>
2023-05-30 18:50:24 +0300

    Добавил админку

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from app.extension import db, migrate
+from app.extension import db, migrate, Admin, ModelView
 
 from config import Config
 from .posts import posts
+
+from app.posts import models
 
 
 app = Flask(__name__)
@@ -13,10 +15,16 @@ app.config.from_object(Config)
 db.init_app(app)
 migrate.init_app(app, db)
 
+admin = Admin(app, name="Blog (Red Eye)", template_mode="bootstrap4")
+admin.add_view(ModelView(models.Post, db.session))
+admin.add_view(ModelView(models.Tag, db.session))
+
+
+
 # Register blueprints here
 app.register_blueprint(posts, url_prefix="/blog")
 
 
 
 from app import routes
-from app.posts import models
+
--- app/extension.py
+++ app/extension.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+
